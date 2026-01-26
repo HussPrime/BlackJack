c6b5782d26bd4dde9d9a5dccfbc5108754159db4
Hussein Mazyad <MazyadH1@s2.rpn.ch>
2026-01-26 12:09:32 +0100

    Display blackjack table

--- Blackjack/src/App.jsx
+++ Blackjack/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import Blackjack_table from "../public/blackjack_table/Blackjack_table"
+import { Environment, OrbitControls } from "@react-three/drei"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const BlackJack = () => {
+  return(
+    <Canvas>
+      <ambientLight intensity={2}/>
+      <OrbitControls/>
+      <Suspense fallback={null}>
+        <Blackjack_table/>
+      </Suspense>
+      <Environment preset="sunset"/>
+    </Canvas>
   )
 }
 
-export default App
+export default BlackJack
