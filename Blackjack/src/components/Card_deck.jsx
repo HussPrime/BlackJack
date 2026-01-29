4f3176ebccc7184d0d868870e4b14b35ccc8090f
Hussein Mazyad <MazyadH1@s2.rpn.ch>
2026-01-29 22:14:55 +0100

    fix carddeck link

--- Blackjack/src/components/Card_deck.jsx
+++ Blackjack/src/components/Card_deck.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export default function Model({card, position, rotation, props}) {
-  const { nodes, materials } = useGLTF('`${import.meta.env.BASE_URL}card_deck/card_deck.gltf`')
+  const { nodes, materials } = useGLTF(`${import.meta.env.BASE_URL}card_deck/card_deck.gltf`)
 
   if(card){
     return(
